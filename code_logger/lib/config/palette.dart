27aa85407c4849362af94e9ddfa0e4a1ff99429b
choikh0423 <kyuhwanchoi0423@gmail.com>
2022-04-07 02:37:58 +0900

    2022-4-7 Temporary Tab Bar

---
+++ code_logger/lib/config/palette.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class Palette {
+  static const Color backgroundColor = Color(0xFF0C1427);
+  static const Color mainFontColor = Color(0xFFFFFFFF);
+  static const Color subFontColor = Color(0xFFB7B7B7);
+  static const Color IconColor = Color(0xFFFFFFFF);
+  static const Color redIconColor = Color(0xFFDA4747);
+  static const Color greenIconColor = Color(0xFF58D97C);
+  static const Color yellowIconColor = Color(0xFFD0C13B);
+}
