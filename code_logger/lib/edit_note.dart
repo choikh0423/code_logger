8587ca574274420f6703f3e597fcbf1c6179c31a
SeungCholCho <seung.chol.cho@gmail.com>
2022-04-08 02:07:23 +0900

    test

--- code_logger/lib/edit_note.dart
+++ code_logger/lib/edit_note.dart
@@ -8,6 +8,56 @@ class EditNote extends StatefulWidget {
   State<EditNote> createState() => _EditNoteState();
 }
 
+class Note {
+  String _text, _date; //our note contain text,create date and an Id
+  int _id;
+
+  Note.update(this._text, this._date, this._id);
+
+  Note(this._text, this._date);
+
+  Note.map(dynamic obj) {
+    this._text = obj['name'];
+    this._date = obj['date'];
+    this._id = obj['id'];
+  }
+
+  Note.fromMap(Map<String, dynamic> map) {
+    this._text = map['name'];
+    this._id = map['id'];
+    this._date = map['date'];
+  }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = new Map<String, dynamic>();
+
+    map['name'] = this._text;
+    map['date'] = this._date;
+
+    if (id != null) map['id'] = this._id;
+
+    return map;
+  }
+
+  get name => _text;
+
+  set name(value) {
+    _text = value;
+  }
+
+  get date => _date;
+
+  set date(value) {
+    _date = value;
+  }
+
+  int get id => _id;
+
+  set id(int value) {
+    _id = value;
+  }
+}
+
 class _EditNoteState extends State<EditNote> {
   @override
   Widget build(BuildContext context) {
@@ -65,6 +115,7 @@ class _EditNoteState extends State<EditNote> {
             children: [
               SizedBox(height: 10.0),
               Text("Note Title", style: FontStyle.heading2TextStyle),
+
               SizedBox(height: 30.0),
               Text(
                 "Note Topics",
