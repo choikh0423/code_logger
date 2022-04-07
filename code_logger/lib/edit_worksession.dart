97702bfd3924e506d9d043f7cf489e505d7a2158
SeungCholCho <seung.chol.cho@gmail.com>
2022-04-07 23:59:30 +0900

    i donno wat im doin

--- code_logger/lib/edit_worksession.dart
+++ code_logger/lib/edit_worksession.dart
@@ -17,7 +17,7 @@ class _EditSessionState extends State<EditSession> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: ElevatedButton(
-              child: Text("hello"),
+              child: Text("back"),
               onPressed: () {
                 Navigator.pop(context);
               },
